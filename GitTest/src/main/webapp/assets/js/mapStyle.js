1b239641a3f557ea401e2ae720f6e5552a6079e9
lkjin5064 <lkjin5064@gmail.com>
2023-02-26 02:20:05 +0900

    map 레이아웃 추가

--- GitTest/src/main/webapp/assets/js/mapStyle.js
+++ GitTest/src/main/webapp/assets/js/mapStyle.js
@@ -13,17 +13,7 @@ $('#up_btn').click(()=>{
 	$('#down_btn').css('display', 'block')
 })
 
-// 버튼 클릭시 색상 유지 -> 0 : 일단 이것도 다시 다시
-// 다른 버튼 클릭시 다른 버튼에 색상을 유지
-// 한번더 누르면 없어지도록
-$('.category_btn button').on('mousedown', (id_name)=>{
-	if(id_name.target.id === 'rs_btn'){
-		console.log('매물버튼')
-		$('.category_btn btn').css({'background-color':'white'})
-		
-		$('#rs_btn').css({'background-image':'linear-gradient(195deg, #66BB6A 0%, #43A047 100%)'})
-	}else if(id_name.target.id === 'cs_btn'){
-		$('#cs_btn').css({'background-image':'linear-gradient(195deg, #66BB6A 0%, #43A047 100%)'})
-		
-	}
-})
+
+
+
+// 리스트 버튼

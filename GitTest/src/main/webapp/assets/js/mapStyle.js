af9cd9cace5201c1a4c3c235247cb9021c4f09c0
yongeun1111 <yongeun1111@gmail.com>
2023-03-03 11:45:13 +0900

    Map.jsp 동 버튼 검색 변경

--- GitTest/src/main/webapp/assets/js/mapStyle.js
+++ GitTest/src/main/webapp/assets/js/mapStyle.js
@@ -244,6 +244,7 @@ function click_cs(target){
 }
 
 // 구, 동 선택 select
+// 구, 동 선택 select
 $(".dropdown_gu").click(function() {
 	let gu_name = { gu_name: $(this).attr('for') }
 	$("#dropdownMenuButton_gu").text($(this).attr('for'));
@@ -264,6 +265,13 @@ $(".dropdown_gu").click(function() {
 				let dong_option = $('<input type="radio" name="area" id=' + dong_code + ' class="dropdown-item btn-check"><label for="' + dong_code + '" class="dropdown_dong drop-btn ms-0 mb-0">' + dong_name + '</label>')
 				$('#choice_dong').append(dong_option);
 			}
+			$(".dropdown_dong").click(function(){
+				console.log("동버튼클릭")
+				$("#dropdownMenuButton_dong").text($(this).text())
+				$("#dropdownMenuButton_dong").removeClass("btn-outline-success")
+				$("#dropdownMenuButton_dong").addClass("btn-success")
+
+			})
 			// 구 클릭후 동 불러오기 완료시 동버튼 활성화
 			$("#dropdownMenuButton_dong").removeAttr("disabled");
 		},
